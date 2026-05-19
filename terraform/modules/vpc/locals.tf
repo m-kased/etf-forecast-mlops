2cca2978dc859fcd69c01cae9a8c6e82037c213c
m-kased <31375621+m-kased@users.noreply.github.com>
2026-05-19 13:04:09 +0300

    Add VPC module with subnets, NAT gateways and route tables

---
+++ terraform/modules/vpc/locals.tf
@@ -0,0 +1,11 @@
+locals {
+  name_prefix = "${var.project_name}-${var.environment}"
+
+  nat_gateway_count = var.single_nat_gateway ? 1 : length(var.availability_zones)
+
+  common_tags = merge(var.tags, {
+    Project     = var.project_name
+    Environment = var.environment
+    ManagedBy   = "terraform"
+  })
+}
